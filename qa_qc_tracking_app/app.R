719b1c60dc2f8b899e482e98477108ffb7522613
Farshad Ebrahimi <Farshad.Ebrahimi@phila.gov>
2024-07-19 15:02:38 -0400

    gap and postcon

--- qa_qc_tracking_app/app.R
+++ qa_qc_tracking_app/app.R
@@ -158,6 +158,11 @@ server <- function(input, output, session) {
     }
   })
   
+  
+  # postcon table
+  postcon_table <- odbc::dbGetQuery(poolConn, "SELECT * FROM fieldwork.tbl_postcon_status") %>%
+    mutate(postcon_exist = "Yes")
+    
   #query the collection calendar and arrange by deployment_uid
   collect_query <- "select *, admin.fun_smp_to_system(smp_id) as system_id ,data.fun_date_to_fiscal_quarter(cast(date_100percent AS DATE)) as expected_fiscal_quarter, data.fun_date_to_fiscal_quarter(cast(collection_dtime_est AS DATE)) as collected_fiscal_quarter from fieldwork.viw_qaqc_deployments"
 
@@ -174,6 +179,9 @@ server <- function(input, output, session) {
   gw_data_day <- odbc::dbGetQuery(poolConn,"SELECT * FROM data.mat_gw_data_day") %>%
     mutate(gw_data_exist = "Yes")
 
+  # data gaps
+  datagaps <- odbc::dbGetQuery(poolConn, "SELECT * FROM data.tbl_datagaps")
+  
   # populating the main table
   rv$collect_table_db <- reactive( odbc::dbGetQuery(poolConn, collect_query) %>%
     mutate(fiscal_quarter = ifelse(collected_fiscal_quarter == "", expected_fiscal_quarter, collected_fiscal_quarter)) %>%
@@ -217,6 +225,8 @@ server <- function(input, output, session) {
 
   #arrange and filtered the collection calendar
   rv$collect_table_filter <- reactive(rv$collect_table_db() %>% 
+                                        left_join(datagaps, by = "deployment_uid") %>%
+                                        left_join(postcon_table, by = "system_id") %>%
                                         mutate(collection_dtime_est = collection_dtime_est %>% lubridate::ymd()) %>%
                                         #use 1 or 0 for public or private, respectively, and 0.5 for both, with a tolerance of .51 
                                         #so if .5 is selected, 0 and 1 are picked up
@@ -233,13 +243,13 @@ server <- function(input, output, session) {
                                                       ~ case_when(. == 1 ~ "Baro",
                                                                   . == 2 ~ "Level",
                                                                   . == 3 ~ "Data Logger"))) %>%
+                                        replace_na(list(postcon_exist = "No")) %>%
                                         distinct()
-                                      
                                         ) 
   
   #select and rename columns to show in app
   rv$collect_table <- reactive(rv$collect_table_filter() %>%
-                                 select(`SMP ID` = smp_id, `OW Suffix`= ow_suffix, `Project Name` = project_name, Term = term, `Collection Date` = collection_status, `Data in DB?` = qa_qc, `QA/QC Status` = status, `System Flagged?` = flagged, deployment_uid) %>%
+                                 select(`SMP ID` = smp_id, `OW Suffix`= ow_suffix, `Project Name` = project_name, Term = term, `Collection Date` = collection_status, `Data in DB?` = qa_qc, `QA/QC Status` = status, `Post-Con Status?` = postcon_exist, `Flagged?` = flagged, `Gap Days` = datagap_days ,deployment_uid) %>%
                                  distinct()#, Notes =  qaqc_notes)
   )
                           
@@ -313,7 +323,7 @@ server <- function(input, output, session) {
                 #`System ID` = colDef(width = 90),
                 `SMP ID` = colDef(width = 95),
                 `OW Suffix` = colDef(width = 100),
-                `Project Name` = colDef(width = 350),
+                `Project Name` = colDef(width = 250),
                  Term = colDef(width = 75),
                 #`Collected/Expected Quarter` = colDef(width = 225),
                 `Collection Date` = colDef(width = 125, style = function(value){
@@ -358,7 +368,7 @@ server <- function(input, output, session) {
                   }
                   list(backgroundColor = color, color = textColor, fontweight = "bold")
                 }),
-                `System Flagged?` = colDef(width = 200, style = function(value){
+                `Flagged?` = colDef(width = 75, style = function(value){
                   if(!is.na(value) & value == "No"){
                     color = "green"
                     textColor = "white"
