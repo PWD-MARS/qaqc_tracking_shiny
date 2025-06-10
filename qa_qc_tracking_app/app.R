543a23e3f03c8f5ba7a5988b29bd299a47fd206d
Jon Bryant <jon@jonbryant.co>
2025-06-10 15:11:53 -0400

    updated app to work with dtime
    Closes #1

--- qa_qc_tracking_app/app.R
+++ qa_qc_tracking_app/app.R
@@ -47,7 +47,7 @@ options(DT.options = list(pageLength = 25))
 # gets environmental variables saved in local or pwdrstudio environment
 # poolConn <- dbPool(odbc(), dsn = "mars14_datav2", uid = Sys.getenv("shiny_uid"), pwd = Sys.getenv("shiny_pwd"))
 poolConn <- dbPool(RPostgres::Postgres(),
-  dbname = "sandbox_dtime",
+  dbname = "mars_prod",
   host = "PWDMARSDBS1",
   port = 5434,
   user = Sys.getenv("shiny_uid"),
